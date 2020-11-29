94f49bcf61bec9306848127cd59c69598f7e9f57
utari24 <utaritari100@gmail.com>
2020-11-29 20:45:43 +0700

    timepicker

---
+++ app/src/main/java/com/kita/skdnews/SplashActivity.java
@@ -0,0 +1,31 @@
+package com.kita.skdnews;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashActivity extends AppCompatActivity {
+    private static String TAG = SplashActivity.class.getSimpleName();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        initApp();
+    }
+
+    private void initApp(){
+        View view = findViewById(R.id.splash_view);
+        view.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent();
+                setResult(RESULT_OK, intent);
+                finish();
+            }
+        });
+    }
+}
